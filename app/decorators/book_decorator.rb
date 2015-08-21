9596ee149f81a64f1c4b41a90ac18d17666347ca
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-08-21 18:50:23 +0200

    Change status when user can add to cart

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -2,7 +2,15 @@ class BookDecorator < Draper::Decorator
   delegate_all
 
   def status
-    available_quantity == 1 ? false : true
+    if available_quantity == 1 || user_already_have_such_category_in_cart?
+      false
+    else
+      true
+    end
+  end
+
+  def user_already_have_such_category_in_cart?
+    h.current_user.cart.books.map { |book| book.book_category }.include?(book_category)
   end
 
   def available_quantity
