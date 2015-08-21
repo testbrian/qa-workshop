641638b861f8fd13ee4c39cb928a5e6ddf19029e
testbrian <brian.burns@netguru.co>
2015-08-21 19:34:42 +0200

    Admin role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821171037) do
+ActiveRecord::Schema.define(version: 20150821173404) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admins", ["name", "resource_type", "resource_id"], name: "index_admins_on_name_and_resource_type_and_resource_id"
+  add_index "admins", ["name"], name: "index_admins_on_name"
 
   create_table "book_categories", force: :cascade do |t|
     t.string "name"
@@ -75,5 +86,12 @@ ActiveRecord::Schema.define(version: 20150821171037) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "users_admins", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "admin_id"
+  end
+
+  add_index "users_admins", ["user_id", "admin_id"], name: "index_users_admins_on_user_id_and_admin_id"
 
 end
