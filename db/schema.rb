5e27b6eb1494bd4e04c2daaa50736dc07f5321a8
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-08-21 12:31:50 +0200

    Add BookCategories table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821101846) do
+ActiveRecord::Schema.define(version: 20150821102829) do
+
+  create_table "book_categories", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string  "author"
