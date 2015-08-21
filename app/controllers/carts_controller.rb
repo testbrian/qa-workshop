dea013ff8e7ce3c6a65689406306665c567cc259
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-08-21 17:37:26 +0200

    Why so searious?

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,6 +4,6 @@ class CartsController < ApplicationController
 
   def add_book
     current_user.cart.books << book
-    redirect_to root_path, notice: 'Book added to cart!'
+    redirect_to root_path, notice: 'Book removed from cart!'
   end
 end
