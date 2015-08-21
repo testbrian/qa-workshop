2777f5243a41ee8b59ee6e34e4dca0605626002c
Kamil Ejsymont <kamil.ejsymont@netguru.pl>
2015-08-21 19:32:57 +0200

    Tables styling

--- app/decorators/order_decorator.rb
+++ app/decorators/order_decorator.rb
@@ -2,14 +2,12 @@ class OrderDecorator < Draper::Decorator
   delegate_all
 
   def librarian_action(order)
-    css = 'btn btn-primary'
+    css = 'btn btn-success btn-xs'
     case state
     when 'awaiting_collection'
       h.link_to 'Collect', h.collect_order_path(order), class: css, method: :post
     when 'collected'
       h.link_to 'Return', h.return_order_path(order), class: css, method: :post
-    when 'returned'
-      'returned'
     end
   end
 end
