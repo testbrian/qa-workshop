6b36f79c072d543cdda40e8b7b62e9bfe0fa24f5
testbrian <brian.burns@netguru.co>
2015-08-21 20:28:00 +0200

    major fix

--- db/migrate/20150821105358_create_cart.rb
+++ db/migrate/20150821105358_create_cart.rb
@@ -1,7 +1,7 @@
 class CreateCart < ActiveRecord::Migration
   def change
     create_table :carts do |t|
-      t.references :user, index: true, foreign_key: true
+      t.integer :user_id, index: true
     end
   end
 end
