ba50c3d98c29cca2790d96018bde2df7add1fc2c
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-08-21 15:27:13 +0200

    Create cart at save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
+  after_save :create_cart_if_needed
+
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable
 
   has_one :cart, dependent: :destroy
+
+  private
+  def create_cart_if_needed
+    create_cart if cart.nil?
+  end
 end
