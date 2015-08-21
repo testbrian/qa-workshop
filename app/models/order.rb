9120e68b005596561cb794f1a903777f7e823e16
testbrian <brian.burns@netguru.co>
2015-08-21 19:15:22 +0200

    update quantity on return

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,7 +8,12 @@ class Order < ActiveRecord::Base
     end
 
     event :return do
-      transition all => :returned
+      transition all => :returned do |order|
+        order.cart.books.each do |book|
+          book.borrowed_quantity -= 1
+          book.save
+        end
+      end
     end
   end
 end
