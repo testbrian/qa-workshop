cd7b71ee16003148f74b87fc0e5afc2c988e270d
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-08-21 13:20:35 +0200

    Add carts association to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
   validates :author, :title, :publishing_house, :published_at, :quantity, :book_category, presence: true
   belongs_to :book_category
+  has_many :carts
 end
