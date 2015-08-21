629d37189301644febf666ebd60c6ff26397c220
testbrian <brian.burns@netguru.co>
2015-08-21 15:54:55 +0200

    remove uploader

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,4 @@ class Book < ActiveRecord::Base
   validates :author, :title, :publishing_house, :published_at, :quantity, :book_category, presence: true
   belongs_to :book_category
   has_and_belongs_to_many :carts
-  mount_uploader :cover, CoverUploader
 end
