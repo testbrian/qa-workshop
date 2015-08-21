d9711374728a8ca693553ad031d8ede836c8da79
testbrian <brian.burns@netguru.co>
2015-08-21 18:22:41 +0200

    remove admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,6 +1,6 @@
-RailsAdmin.config do |config|
-  config.authenticate_with do
-    warden.authenticate! scope: :user
-  end
-  config.current_user_method(&:current_user)
-end
+# RailsAdmin.config do |config|
+#   config.authenticate_with do
+#     warden.authenticate! scope: :user
+#   end
+#   config.current_user_method(&:current_user)
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home' => 'home#index'
   root 'home#index'
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   resources :carts, only: [:index] do
     post 'add_book', on: :member
