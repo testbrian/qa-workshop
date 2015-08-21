fa05304ba83ff7cfec7c44c3222a60e3ba73b017
Kamil Ejsymont <kamil.ejsymont@netguru.pl>
2015-08-21 13:08:16 +0200

    Bootstrap import

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require turbolinks
+//= require bootstrap-sprockets
 //= require bootstrap-material-design
+//= require_tree .
