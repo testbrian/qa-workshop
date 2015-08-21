4cd054b32f7cb586081d087016c6aebbcfca9b7e
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-08-21 17:26:15 +0200

    More and more and more

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-  validates :author, :title, :publishing_house, :published_at, :quantity, :book_category, presence: true
+  validates :author, :publishing_house, :published_at, :quantity, :book_category, presence: true
   belongs_to :book_category
   has_and_belongs_to_many :carts
 end
