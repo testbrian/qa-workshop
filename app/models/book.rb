59abfbc6e72ae7f5236188c6987c866a90812e7d
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2015-08-21 14:54:19 +0200

    Add CoverUploader and mount it in Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ActiveRecord::Base
   validates :author, :title, :publishing_house, :published_at, :quantity, :book_category, presence: true
   belongs_to :book_category
   has_and_belongs_to_many :carts
+  mount_uploader :cover, CoverUploader
 end
