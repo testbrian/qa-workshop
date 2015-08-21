7bf074beb492a2916d444c1ce9be2f2535e8ed48
testbrian <brian.burns@netguru.co>
2015-08-21 19:08:58 +0200

    link to correct place

--- app/decorators/order_decorator.rb
+++ app/decorators/order_decorator.rb
@@ -1,13 +1,13 @@
 class OrderDecorator < Draper::Decorator
   delegate_all
 
-  def librarian_action
+  def librarian_action(order)
     css = 'btn btn-primary'
     case state
     when 'awaiting_collection'
-      h.link_to 'Collect', '#', class: css
+      h.link_to 'Collect', h.collect_order_path(order), class: css, method: :post
     when 'collected'
-      h.link_to 'Return', '#', class: css
+      h.link_to 'Return', h.return_order_path(order), class: css, method: :post
     when 'returned'
       'returned'
     end
