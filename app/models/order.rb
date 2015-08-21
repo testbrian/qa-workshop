752cee1f5c11a125a15d25646a5dde07e210f2ed
testbrian <brian.burns@netguru.co>
2015-08-21 19:00:22 +0200

    some states;

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,14 @@
 class Order < ActiveRecord::Base
   belongs_to :cart
   belongs_to :user
+
+  state_machine initial: :awaiting_collection do
+    event :collect do
+      transition all => :collected
+    end
+
+    event :return do
+      transition all => :returned
+    end
+  end
 end
